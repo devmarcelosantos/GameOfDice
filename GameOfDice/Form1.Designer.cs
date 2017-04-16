c1052a531f7be786bca40709f67c8d6a16c103f0
Marcelo Santos <Marcelo Santos>
2017-04-16 02:10:41 -0300

    Adicionar arquivos de projeto.

---
+++ GameOfDice/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace GameOfDice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(55, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 78);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Player 1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Location = new System.Drawing.Point(55, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(191, 78);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Player 2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "CLIQUE NOS BOTÕES ABAIXO PARA JOGAR";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(294, 294);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "JOGO DE DADOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GameOfDice/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfDice
+{
+    public partial class Form1 : Form
+    {
+        int player_one, player_two, sum_one, sum_two, rounds;
+        int count_one;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Random round = new Random();
+            player_one = round.Next(1, 7);
+            MessageBox.Show(" Numero: " + player_one);
+            sum_one += player_one;
+            rounds++;
+            count_one++;
+            if (count_one == 3)
+            {
+                MessageBox.Show("Time of the second player !");
+            }
+            if (rounds == 6)
+            {
+                premium();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random round = new Random();
+            player_two = round.Next(1, 7);
+            MessageBox.Show(" Numero: " + player_two);
+            sum_two += player_two;
+            rounds++;
+            if (rounds == 6)
+            {
+                premium();
+            }
+        }
+
+        public void premium()
+        {
+            if (sum_one > sum_two)
+            {
+                MessageBox.Show("Player One WINS !!!");
+                rounds = 0;
+                
+            }
+            else if (sum_one < sum_two)
+            {
+                MessageBox.Show("Player Two WINS !!!");
+                rounds = 0;
+            }
+            else
+            {
+                MessageBox.Show("Tie");
+                rounds = 0;
+            }
+        }
+    }
+}
